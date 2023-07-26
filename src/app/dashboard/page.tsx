4de9a01dda6ee8d8f47899b2570433f2a612d928
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-07-26 15:18:04 +0900

    dashboardページに選択した月の全ての日と曜日を表示するようにした

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { useState } from 'react';
+
 export default function Dashboard() {
   const now = new Date()
   const nowYear = now.getFullYear()
@@ -6,10 +9,42 @@ export default function Dashboard() {
   const formattedNowMonth = `${nowYear}-${(nowMonth + 1).toString().padStart(2, '0')}`;
   //const formattedNowDate = `${nowYear}-${(nowMonth + 1).toString().padStart(2, '0')}-${nowDate.toString().padStart(2, '0')}`;
 
+  // ステートとして選択した年と月を管理
+  const [selectedYearMonth, setSelectedYearMonth] = useState(formattedNowMonth);
+
+  // カレンダーの日付と曜日を計算するヘルパー関数
+  const getCalendarDates = (year, month) => {
+    const firstDayOfMonth = new Date(year, month, 1);
+    const lastDayOfMonth = new Date(year, month + 1, 0);
+    const dates = [];
+    const dayLabels = ['日', '月', '火', '水', '木', '金', '土'];
+
+    for (let date = new Date(firstDayOfMonth); date <= lastDayOfMonth; date.setDate(date.getDate() + 1)) {
+      const dayOfWeek = dayLabels[date.getDay()];
+      dates.push({ date: date.getDate(), dayOfWeek });
+    }
+
+    return dates;
+  };
+
+  // 選択した年と月に対応するカレンダーの日付と曜日を取得
+  const calendarDates = getCalendarDates(
+    parseInt(selectedYearMonth.split('-')[0]),
+    parseInt(selectedYearMonth.split('-')[1]) - 1
+  );
+
   return (
     <>
       <div>Dashboard</div>
-      <input type="month" defaultValue={formattedNowMonth}></input>
+      <input type="month" value={selectedYearMonth} onChange={(e) => setSelectedYearMonth(e.target.value)} />
+      <hr />
+      <div>
+        {calendarDates.map((item) => (
+          <div key={item.date}>
+            <span>{item.date}</span> <span>{item.dayOfWeek}</span>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
