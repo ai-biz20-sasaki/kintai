ce2c328b76f943af9f08f713cfc4807eec0537de
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-07-26 14:10:09 +0900

    dashboardページのカレンダーのデフォルトに今月を表示するようにした

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
-import Link from 'next/link'
-
 export default function Dashboard() {
+  const now = new Date()
+  const nowYear = now.getFullYear()
+  const nowMonth = now.getMonth()
+  //const nowDate = now.getDate()
+  const formattedNowMonth = `${nowYear}-${(nowMonth + 1).toString().padStart(2, '0')}`;
+  //const formattedNowDate = `${nowYear}-${(nowMonth + 1).toString().padStart(2, '0')}-${nowDate.toString().padStart(2, '0')}`;
+
   return (
-    <div>Dashboard</div>
+    <>
+      <div>Dashboard</div>
+      <input type="month" defaultValue={formattedNowMonth}></input>
+    </>
   )
 }
