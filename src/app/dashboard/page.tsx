ca82e0a2b567482449eff24a4753868a4d9d8a05
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-07-29 18:29:11 +0900

    開始時間、終了時間、休憩開始時間、休憩終了時間の入力欄を追加し可変にした

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,12 +9,25 @@ export default function Dashboard() {
   const formattedNowMonth = `${nowYear}-${(nowMonth + 1).toString().padStart(2, '0')}`;
   //const formattedNowDate = `${nowYear}-${(nowMonth + 1).toString().padStart(2, '0')}-${nowDate.toString().padStart(2, '0')}`;
 
+  // ステートとして選択した年と月を管理
+  const [selectedYearMonth, setSelectedYearMonth] = useState(formattedNowMonth);
+
+  // ステートとして開始時間、終了時間、休憩開始時間、休憩終了時間を管理
+  const [startHour, setStartHour] = useState(9);
+  const [startMinute, setStartMinute] = useState(30);
+  const [endHour, setEndHour] = useState(18);
+  const [endMinute, setEndMinute] = useState(30);
+  const [breakStartHour, setBreakStartHour] = useState(12);
+  const [breakStartMinute, setBreakStartMinute] = useState(0);
+  const [breakEndHour, setBreakEndHour] = useState(13);
+  const [breakEndMinute, setBreakEndMinute] = useState(0);
+
   //開始時間、終了時間、休憩開始時間、休憩終了時間を定義
-  const startTime = new Date(nowYear, nowMonth, nowDate, 9, 30, 0)  //9:30
-  const endTime =  new Date(nowYear, nowMonth, nowDate, 18, 30, 0)  //18:30
+  const startTime = new Date(nowYear, nowMonth, nowDate, startHour, startMinute, 0)  //9:30
+  const endTime =  new Date(nowYear, nowMonth, nowDate, endHour, endMinute, 0)  //18:30
   const leaveEarlyTime =  new Date(nowYear, nowMonth, nowDate, 16, 0, 0)  //16:00
-  const breakStartTime = new Date(nowYear, nowMonth, nowDate, 12, 0, 0) //12:00
-  const breakEndTime = new Date(nowYear, nowMonth, nowDate, 13, 0, 0) //13:00
+  const breakStartTime = new Date(nowYear, nowMonth, nowDate, breakStartHour, breakStartMinute, 0) //12:00
+  const breakEndTime = new Date(nowYear, nowMonth, nowDate, breakEndHour, breakEndMinute, 0) //13:00
 
   // ミリ秒をhh:mm形式に変換するヘルパー関数
   const convertMsToHHMM = (ms: number) => {
@@ -28,15 +41,12 @@ export default function Dashboard() {
     return `${formattedHours}:${formattedMinutes}`;
   };
 
-// 時（hh）と分（mm）を取得し、文字列として整形するヘルパー関数
-const formatHHMM = (date: Date) => {
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  return `${hours}:${minutes}`;
-};
-
-  // ステートとして選択した年と月を管理
-  const [selectedYearMonth, setSelectedYearMonth] = useState(formattedNowMonth);
+  // 時（hh）と分（mm）を取得し、文字列として整形するヘルパー関数
+  const formatHHMM = (date: Date) => {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  };
 
   // カレンダーの日付と曜日を計算するヘルパー関数
   const getCalendarDates = (year: number, month: number) => {
@@ -124,7 +134,34 @@ const formatHHMM = (date: Date) => {
   return (
     <div className="m-3">
       <div>Dashboard</div>
+      {/* 対象年月を選択 */}
       <input type="month" value={selectedYearMonth} onChange={(e) => setSelectedYearMonth(e.target.value)} />
+
+      <div>
+        {/* 開始時間を入力 */}
+        <label htmlFor="startTime">開始時間：</label>
+        <input value={startHour} onChange={(e) => setStartHour(Number(e.target.value))} size={1}></input>      
+        <input value={startMinute} onChange={(e) => setStartMinute(Number(e.target.value))} size={1}></input>      
+      </div>
+      <div>
+        {/* 終了時間を入力 */}
+        <label htmlFor="endTime">終了時間：</label>
+        <input value={endHour} onChange={(e) => setEndHour(Number(e.target.value))} size={1}></input>      
+        <input value={endMinute} onChange={(e) => setEndMinute(Number(e.target.value))} size={1}></input>      
+      </div>
+      <div>
+        {/* 休憩開始時間を入力 */}
+        <label htmlFor="breakStartTime">休憩開始時間：</label>
+        <input value={breakStartHour} onChange={(e) => setBreakStartHour(Number(e.target.value))} size={1}></input>      
+        <input value={breakStartMinute} onChange={(e) => setBreakStartMinute(Number(e.target.value))} size={1}></input>      
+      </div>
+      <div>
+        {/* 休憩終了時間を入力 */}
+        <label htmlFor="breakEndTime">休憩終了時間：</label>
+        <input value={breakEndHour} onChange={(e) => setBreakEndHour(Number(e.target.value))} size={1}></input>      
+        <input value={breakEndMinute} onChange={(e) => setBreakEndMinute(Number(e.target.value))} size={1}></input>      
+      </div>
+      
       <hr className="my-2" />
       <div>
         {calendarDates.map((item) => (
